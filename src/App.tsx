8f4d86fd2058d1430830d6a368a543dc5e020d1d
Shane Bird <shane.bird88@gmail.com>
2022-09-01 21:07:55 +0000

    Added Tailwind Css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import plu1 from "../assets/plu1.jpg";
+import plu4767 from "../assets/plu4767.jpg";
+import "./global.css";
+
 function App() {
   return (
     <div className='App'>
-      <div>This is a brand new site under construction using Vite and React</div>
+      <h1 className='mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
+        WOOLWORTHS PLU FILE LINKS:
+      </h1>
+      <div className='gap-100'>
+        <button
+          onClick={() =>
+            (window.location.href =
+              "https://birdey.com.au/content/dam/emergency-documents/bakeryMethodsheets/what_good_looks_like/plu1.jpg")
+          }
+          type='button'
+          className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
+          <div className='text-lg'>PLU - 1</div>
+          <img style={{ height: 200 }} src={plu1} />
+        </button>
+        <button
+          onClick={() =>
+            (window.location.href =
+              "https://birdey.com.au/content/dam/emergency-documents/bakeryMethodsheets/what_good_looks_like/plu4767.jpg")
+          }
+          type='button'
+          className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
+          <div className='text-lg'>PLU - 4767</div>
+          <img style={{ height: 200 }} src={plu4767} />
+        </button>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.cjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      gray: colors.gray,
+      blue: colors.sky,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  plugins: [],
+};
